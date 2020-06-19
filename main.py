e380089e2509dccabdb29059c2215a1aeaa3946b
Jaap Overeem <110018@st-maartenscollege.nl>
2020-06-19 10:52:59 +0000

    Glag mannetje gemaakt

--- main.py
+++ main.py
@@ -7,3 +7,16 @@ print ("Hallo, " + name, "welkom bij galgje!")
 print("")
 
 woorden = ["auto", "fiets", "vrachtwagen", "vliegtuig", "deurklink", "zandloper", "euro", "afrika", "europa", "amerika", "rusland", "china", "loopfiets"]
+
+galg = [
+  "---------",
+  "|       |",
+  "|       O",
+  "|       |",
+  "|      -+-",
+  "|       |",
+  "|      / \\",
+  "|",
+  "|",
+  "------------"
+]
