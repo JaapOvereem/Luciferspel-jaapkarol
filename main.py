be68dd15034b578edbe8ad0371d990a80fe4ceb6
Jaap Overeem <110018@st-maartenscollege.nl>
2020-06-26 09:28:36 +0000

    spatie bij welke naam wil je gebruiken

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import time
 import random
 
-name = input("Welke naam wil je gebruiken?")
+name = input("Welke naam wil je gebruiken? ")
 print ("Hallo, " + name, "welkom bij galgje!")
 
 print("")
